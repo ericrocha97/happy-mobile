e735d04a380da0f769bebff736a0b23e7fa4f3d1
Eric Rocha <ericrochaeric@gmail.com>
2020-10-15 14:14:51 -0300

    init

---
+++ src/pages/OrphanagesDetails.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function OrphanagesDetails() {
+    return (
+        <View style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <Text style={{
+               color: '#000',
+               fontFamily: 'Nunito_700Bold',
+               fontSize: 50
+            }}>Teste</Text>
+        </View>
+    )
+}
